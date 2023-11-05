84ac954e9420e5a120197eef6b96ad13adad4f9e
Mitchell Jones <mjone47@uic.edu>
2023-11-05 08:12:04 -0600

    added functionality for adding a note to the window/note list

--- frontend/js/notes.js
+++ frontend/js/notes.js
@@ -1,3 +1,25 @@
+const capturedNote = document.getElementById("writingArea");
+capturedNote.addEventListener("keydown", function (event) {
+    if (event.key === "Enter") {
+        event.preventDefault();
+        addNoteToList();
+        
+    }
+});
+
+function addNoteToList() {
+    const capturedNote = document.getElementById("writingArea");
+    const notesList = document.getElementById("notesList");
+    const text = capturedNote.value.trim();
+    if (text !== "") {
+        const newNote = document.createElement("li");
+        newNote.textContent = text;
+        notesList.appendChild(newNote);
+        notesList.scrollTop = notesList.scrollHeight;
+        capturedNote.value = "";
+    }
+}
+
 // shows/hides notes window
 function toggleNotesWindow() {
     var noteWindowStatus = document.getElementById("notesWindow");
