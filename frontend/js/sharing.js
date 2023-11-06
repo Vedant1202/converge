2a161ae4043ea603cc57b05bfb5b928b72945faf
Manuel I <112410801+ManuelIrungaray@users.noreply.github.com>
2023-11-06 13:17:19 -0600

    Update sharing.js

--- frontend/js/sharing.js
+++ frontend/js/sharing.js
@@ -3,9 +3,9 @@ const popupContainer = document.getElementById("popup-container");
 const closeButton = document.getElementById("close-button");
 
 popupButton.addEventListener("click", () => {
-  popupContainer.style.right = "50px"; // Slide the pop-up in from the right
+  popupContainer.style.right = "50px"; 
 });
 
 closeButton.addEventListener("click", () => {
-  popupContainer.style.right = "-400px"; // Slide the pop-up out to the right
+  popupContainer.style.right = "-400px"; 
 });
