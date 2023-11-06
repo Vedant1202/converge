6042c821e5ac4ef76e27f5bfb97ef0e44da23022
Manuel I <112410801+ManuelIrungaray@users.noreply.github.com>
2023-11-06 17:13:40 -0600

    Update sharing.js

--- frontend/js/sharing.js
+++ frontend/js/sharing.js
@@ -1,10 +1,10 @@
-const popupButton = document.getElementById("popup-button");
+//const popupButton = document.getElementById("popup-button");
 const popupContainer = document.getElementById("popup-container");
 const closeButton = document.getElementById("close-button");
 
-popupButton.addEventListener("click", () => {
-  popupContainer.style.right = "50px"; 
-});
+// popupButton.addEventListener("click", () => {
+//   popupContainer.style.right = "50px"; 
+// });
 
 closeButton.addEventListener("click", () => {
   popupContainer.style.right = "-400px"; 
