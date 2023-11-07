fd221d620fe56c6330ce4ac223e191f07b9a92c2
Manuel I <112410801+ManuelIrungaray@users.noreply.github.com>
2023-11-06 19:57:12 -0600

    Update sharing.js

--- frontend/js/sharing.js
+++ frontend/js/sharing.js
@@ -6,6 +6,20 @@ const closeButton = document.getElementById("close-button");
 //   popupContainer.style.right = "50px"; 
 // });
 
+
+document.addEventListener("DOMContentLoaded", function () {
+    const collaborationButtons = document.querySelectorAll(".collaboration");
+    collaborationButtons.forEach(function (button) {
+        button.addEventListener("click", function (event) {
+            event.preventDefault();
+            // Your custom code for handling the "Collaboration" button click event
+            console.log("Collaboration button clicked!");
+          popupContainer.style.right = "50px";
+            // Add your code here to open the collaboration menu or perform any desired action
+        });
+    });
+});
+
 closeButton.addEventListener("click", () => {
   popupContainer.style.right = "-400px"; 
 });
