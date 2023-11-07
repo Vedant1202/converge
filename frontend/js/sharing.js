21bf14bb33fed8f30928ac1f7d3162653fdf3c3a
Manuel I <112410801+ManuelIrungaray@users.noreply.github.com>
2023-11-06 22:13:06 -0600

    Update sharing.js

--- frontend/js/sharing.js
+++ frontend/js/sharing.js
@@ -3,14 +3,12 @@ const popupContainer = document.getElementById("popup-container");
 const closeButton = document.getElementById("close-button");
 
 document.addEventListener("DOMContentLoaded", function () {
-    const collaborationButtons = document.querySelectorAll(".collaboration");
-    collaborationButtons.forEach(function (button) {
-        button.addEventListener("click", function (event) {
-            event.preventDefault();
-            console.log("Collaboration button clicked!");
+    const collaborationButton = [...document.querySelectorAll(".circular-button")].find(button => button.textContent === "🤝");
+    if (collaborationButton) {
+        collaborationButton.addEventListener("click", function () {
           popupContainer.style.right = "50px";
         });
-    });
+    }
 });
 
 closeButton.addEventListener("click", () => {
