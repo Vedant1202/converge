0b586429732c39a4cbb53132cfd1351165cf284b
Vedant Nandoskar <vedant.nandoskar@gmail.com>
2023-11-09 00:24:16 -0600

    assign collab function to new toolbar button

--- frontend/views/canvas/sharing.js
+++ frontend/views/canvas/sharing.js
@@ -2,15 +2,23 @@
 const popupContainer = document.getElementById("popup-container");
 const closeButton = document.getElementById("close-button");
 
-document.addEventListener("DOMContentLoaded", function () {
-    const collaborationButton = [...document.querySelectorAll(".circular-button")].find(button => button.textContent === "🤝");
-    if (collaborationButton) {
-        collaborationButton.addEventListener("click", function () {
-          popupContainer.style.right = "50px";
-        });
+// document.addEventListener("DOMContentLoaded", function () {
+//   const collaborationButton = document.getElementById('collaborationBtn');
+//   if (collaborationButton) {
+//       collaborationButton.addEventListener("click", function () {
+//           popupContainer.style.right = "50px";
+//       });
+//   }
+// });
+
+$('#collaborationBtn').click(function () {
+  if (popupContainer.style.display === 'none' || popupContainer.style.display === '') {
+    popupContainer.style.display = 'block';
+  } else {
+      popupContainer.style.display = 'none';
     }
-});
+})
 
 closeButton.addEventListener("click", () => {
-  popupContainer.style.right = "-400px"; 
+    popupContainer.style.display = 'none';
 });
